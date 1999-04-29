3879ce12748f23c823a049b2f39363ded8386867
columbus <columbus>
1999-04-29 21:16:35 +0000

    Imported sources.

--- acconfig.h
+++ acconfig.h
@@ -1,4 +1,4 @@
-/*      $Id: acconfig.h,v 1.1 1999/04/29 21:16:40 columbus Exp $      */
+/*      $Id: acconfig.h,v 1.1.1.1 1999/04/29 21:16:40 columbus Exp $      */
 
 /*
  *  are you editing the correct file?
