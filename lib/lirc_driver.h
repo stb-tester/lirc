4b3405a54ff9070f4cd9fca742c2973265c23fc7
Alec Leamas <leamas.alec@gmail.com>
2014-03-12 00:20:54 +0100

    lib: Adding a driver library.

---
+++ lib/lirc_driver.h
@@ -0,0 +1,19 @@
+/*
+ * lirc.h - linux infrared remote control header file
+ * last modified 2010/06/03 by Jarod Wilson
+ */
+
+#ifndef _LIRC_DRIVER_H
+#define _LIRC_DRIVER_H
+
+#include "lirc/ir_remote_types.h"
+#include "lirc/lirc_log.h"
+#include "lirc/lirc_options.h"
+#include "lirc/hardware.h"
+#include "lirc/ir_remote.h"
+#include "lirc/receive.h"
+#include "lirc/serial.h"
+#include "lirc/transmit.h"
+#include "lirc/ciniparser.h"
+
+#endif
