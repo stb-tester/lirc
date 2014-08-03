c8f06238dcd27f95174ee21ec970e0f271b3a916
Alec Leamas <leamas@nowhere.net>
2014-08-03 18:52:16 +0200

    autogen.sh: Bugfix.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/bash
 rm -rf aclocal.m4 config.guess config.h.in* config.sub configure depcomp install-sh install.sh
 rm -rf ltmain.sh missing autom4te.cache
 find . -name Makefile -delete
