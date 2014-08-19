8ba9a20216ddc65abd59371a68b8e42169d8c109
Alec Leamas <leamas.alec@gmail.com>
2014-08-19 11:43:03 +0200

    configure.ac et. el.: Remove --enable-maintainer-mode configuration, cleanup.
    Still more cruft to remove. configure.ac now < 400 lines!

--- contrib/lirc-codecs-regression-test.sh
+++ contrib/lirc-codecs-regression-test.sh
@@ -2,7 +2,8 @@
 #
 # This script is used for lirc protocol encode/decode regression testing
 # between two versions of lirc. You'll need two source trees, both fully
-# built, using --enable-debug and --enable-maintainer-mode.
+# built, using --enable-debug and --enable-maintainer-mode for pre-0.9.2;
+# the options are not required for 0.9.2+.
 #
 # Nb: this will generate a LOT of output, both on-screen and in files, so
 # consider running it with 2>/dev/null and in a fresh/empty directory.
@@ -68,10 +69,10 @@ find -L $REMOTES -type f -print0|xargs -0 -n 1 echo|while read;
 do
         # Skip invalid configs with 'one   0  0', see
         # http://sourceforge.net/p/lirc/mailman/message/32297923
-	grep  '  one *0 *0' $REPLY && {
- 		logecho "SKIP:  invalid config (one '0'): $REPLY"
+        grep  '  one *0 *0' $REPLY && {
+                logecho "SKIP:  invalid config (one '0'): $REPLY"
                 continue
-	}
+        }
         if echo $REPLY | grep -Eq $SKIP_PATTERN; then
                 logecho "SKIP: (bad name) : $REPLY"
                 continue
@@ -81,31 +82,31 @@ do
 #
 # send
 #
-	logecho send1
-	logecho "Using remote: $REPLY"
+        logecho send1
+        logecho "Using remote: $REPLY"
         $NEW/daemons/lircd.simsend --pidfile=/tmp/lircd.sim.pid \
                                    --output=/tmp/lircd.sim \
                                    --logfile=$logfile \
-	  			   --nodaemon \
+                                   --nodaemon \
                                    $(get_new_args 'send') \
             $REPLY >${name}.new 2>send1.log || :
         while test -e /tmp/lircd.sim.pid; do sleep .1; done
 
-	logecho send2
+        logecho send2
         $OLD/daemons/lircd.simsend --pidfile=/tmp/lircd.sim.pid \
                                    --output=/tmp/lircd.sim \
                                    --logfile=$logfile \
-				   --nodaemon \
+                                   --nodaemon \
             $REPLY >${name}.old 2>send2.log || :
         while test -e /tmp/lircd.sim.pid; do sleep .1; done
 #
 # receive
 #
-	logecho "rec1 ($REPLY)"
+        logecho "rec1 ($REPLY)"
         $NEW/daemons/lircd.simrec --pidfile=/tmp/lircd.sim.pid \
                                   --output=/tmp/lircd.sim \
                                   --logfile=$logfile \
-				  --nodaemon \
+                                  --nodaemon \
                                   $(get_new_args 'receive') \
             $REPLY <${name}.new >rec1.log 2>&1 &
         while ! tail -5 $logfile | grep ready >/dev/null; do sleep .12; done
@@ -114,11 +115,11 @@ do
         fi
         while test -e /tmp/lircd.sim.pid; do sleep .1; done
 
-	logecho "rec2 ($REPLY)"
+        logecho "rec2 ($REPLY)"
         $OLD/daemons/lircd.simrec --pidfile=/tmp/lircd.sim.pid \
                                   --output=/tmp/lircd.sim \
                                   --logfile=$logfile \
-				  --nodaemon \
+                                  --nodaemon \
             $REPLY <${name}.new >rec2.log 2>&1 &
         while ! tail -1 $logfile | grep ready >/dev/null; do sleep .14; done
         if ! $OLD/tools/irw /tmp/lircd.sim 2>/dev/null >${name}.rec_old; then
