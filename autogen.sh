8c0f600968c02e72aa463ac1ebb80a02f5a321fb
Alec Leamas <leamas.alec@gmail.com>
2014-08-20 11:42:01 +0200

    autogen.sh: Bugfix.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf aclocal.m4 config.guess config.h.in* config.sub configure depcomp install-sh install.sh
 rm -rf ltmain.sh missing autom4te.cache
-find . -name Makefile -delete
+find . \( -name Makefile -a ! -path './drivers/**' \) -delete
 find . -name Makefile.in -delete
 find . -name \*.la -delete
 autoreconf -i -f
