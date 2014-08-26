9d3ae76e570d23ed19eebbc7761c468dd56e6a1b
Alec Leamas <leamas.alec@gmail.com>
2014-08-26 22:47:37 +0200

    Merge lirc_config1.h + lirc_config2.h -> lirc_config.h.

--- lib/lirc_config.h
+++ lib/lirc_config.h
@@ -1,4 +1,27 @@
 
+
+/* daemon socket file names - beneath $varrundir (default /var/run/lirc) */
+#define DEV_LIRCD       "lircd"
+#define DEV_LIRCM       "lircm"
+
+/* config file names - beneath SYSCONFDIR (default /etc) */
+#define CFG_LIRCD       "lircd.conf"
+#define CFG_LIRCM       "lircmd.conf"
+
+/* config file names - beneath $HOME or SYSCONFDIR */
+#define CFG_LIRCRC      "lircrc"
+
+/* log files */
+#define LOG_LIRCD       "lircd"
+#define LOG_LIRMAND     "lirmand"
+
+/* pid file */
+#define PID_LIRCD       "lircd.pid"
+
+/* default port number */
+#define        LIRC_INET_PORT  8765
+
+
 /* Default device in some  places, notably drivers.
  * Might be something else on Darwin(?), but all current
  * Linux systems should be using udev (i. e., not DEVFS).
