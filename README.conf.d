a48d889e8ec829bd88f8ad7f73d7dc68c223edfd
Alec Leamas <alec@nowhere.net>
2014-10-09 23:52:56 +0200

    Install lircd.conf.d + a README, manual update (#56).

---
+++ README.conf.d
@@ -0,0 +1,9 @@
+/etc/lirc/lircd.conf.d README
+=============================
+
+Files in this directory are automatically included, much the same way
+as if theire was a "include" directive for it in lircd.conf. For this to
+work, the filename must match '*.conf'.
+
+You might want to symlink files elseware to this directory,
+naming the link to 'something.conf'.
