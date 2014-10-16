d2e5e522f355e38bda687416437d0787c20ee7d4
Alec Leamas <alec@nowhere.net>
2014-10-17 00:35:43 +0200

    configs: Update definitions of supported capture devices.
    Changes:
      - Capture devices can have supports: = 'bundled' which means that it
        will use remotes with a driver attribute pointing to the capture
        device.
      - lircd_conf: run_select_any_config -> any
      - New note: config file attribute.
      - Update to new lircd_conf filenames.
      - Remove some configurations with missing driver
      - Make all configurations have the same filename as the 'id' attribute.
      - Add the new driver attribute here and there.

--- configs/check_configs.py
+++ configs/check_configs.py
@@ -10,10 +10,12 @@ def main():
     for path in glob.glob('*.conf'):
         with open(path) as f:
             cf = yaml.load(f.read())
+        if cf['config']['id'] + '.conf' != path:
+            print( "Id: %s, path: %s" % (cf['config']['id'], path))
         configs[cf['config']['id']] = cf['config']
     for config in configs:
         if 'supports' in config:
-            if config['supports'] == 'bundled':
+            if config['supports'] == 'lirccode':
                 if not 'lircd_conf' in config:
                     print('bad config:' + config['id'])
     for l in configs.keys():
