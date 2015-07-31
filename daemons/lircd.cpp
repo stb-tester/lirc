8fa53e7fb6732f1c5674120573f5ff492e517183
Alec Leamas <leamas.alec@gmail.com>
2015-07-31 13:58:33 +0200

    lircd: Use poll() instead of select().

--- daemons/lircd.cpp
+++ daemons/lircd.cpp
@@ -341,8 +341,7 @@ int write_socket_len(int fd, const char* buf)
 int read_timeout(int fd, char* buf, int len, int timeout_us)
 {
 	int ret, n;
-	struct pollfd  pfd = {fd, POLLIN, 0}; // fd, events, revents
-	int timeout = timeout_us > 0 ? timeout_us/1000 : -1;
+	struct pollfd  pfd = { .fd = fd, .events = POLLIN, .revents = 0 };
 
 
 	/* CAVEAT: (from libc documentation)
@@ -357,7 +356,7 @@ int read_timeout(int fd, char* buf, int len, int timeout_us)
 	 * waiting as long as there are EINTR.
 	 */
 	do
-		ret = poll(&pfd, 1, timeout);
+		ret = poll(&pfd, 1, timeout * 1000);
 	while (ret == -1 && errno == EINTR);
 	if (ret == -1) {
 		logperror(LIRC_ERROR, "read_timeout: poll() failed");
@@ -2019,7 +2018,7 @@ static int mywaitfordata(unsigned long maxusec)
 					    POLLFDS_SIZE,
 					    tv.tv_sec * 1000 + tv.tv_usec / 1000);
 			else
-				ret = poll((struct pollfd*)&poll_fds.byindex, POLLFDS_SIZE, -1);
+				ret = poll((struct pollfd*)&poll_fds.byindex, POLLFDS_SIZE, 0);
 
 #endif
 			if (ret == -1 && errno != EINTR) {
