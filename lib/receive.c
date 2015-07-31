e911c3255ffc61161c98bac9dc5fa23784e22c3e
Alec Leamas <leamas.alec@gmail.com>
2015-07-31 17:06:13 +0200

    lib: receive: Use poll() instead of select().

--- lib/receive.c
+++ lib/receive.c
@@ -164,9 +164,7 @@ int waitfordata(__u32 maxusec)
 	while (1) {
 		do {
 			do {
-				ret = poll(&pfd, 1, (maxusec > 0) ? (maxusec / 1000) : -1);
-				if (maxusec > 0 && ret == 0)
-					return 0;
+				ret = poll(&pfd, 1, maxusec * 1000);
 			} while (ret == -1 && errno == EINTR);
 			if (ret == -1) {
 				logperror(LIRC_ERROR, "poll() failed");
