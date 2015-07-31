770b0660de6c215ed75e43eba3bad3c8959419f3
Alec Leamas <leamas.alec@gmail.com>
2015-07-31 15:20:43 +0200

    lircrcd: Use poll() instead of select().

--- daemons/lircrcd.cpp
+++ daemons/lircrcd.cpp
@@ -182,8 +182,7 @@ inline int write_socket_len(int fd, const char* buf)
 inline int read_timeout(int fd, char* buf, int len, int timeout_us)
 {
 	int ret, n;
-	struct pollfd  pfd = {fd, POLLIN, 0}; // fd, events, revents
-	int timeout = timeout_us > 0 ? timeout_us/1000 : -1;
+	struct pollfd  pfd = { .fd = fd, .events = POLLIN, .revents = 0 };
 
 	/* CAVEAT: (from libc documentation)
 	 * Any signal will cause `select' to return immediately.  So if your
