a66f0cddd4a21182780c06017a1f7abaea8e3127
Alec Leamas <leamas.alec@gmail.com>
2015-10-01 21:23:56 +0200

    lirc-lsplugins: Fix --plugindir not being respected (#144).

--- tools/lirc-lsplugins.cpp
+++ tools/lirc-lsplugins.cpp
@@ -44,7 +44,7 @@
 	"# Flags:\n" \
 	"# E:\tEmpty: Plugin loaded OK, but is empty (is this a plugin?).\n" \
 	"# F:\tFail: Plugin failed to load (unresolved references?).\n" \
-	"# a:\tAny; Driver can be used with any remote or capture device.\n" \
+	"# a:\tAny: Driver can be used with any remote or capture device.\n" \
 	"# s:\tSend: The driver can send data.\n"
 
 #define LONG_LEGEND \
@@ -400,7 +400,8 @@ static void print_yaml_header(void)
 
 void lsplugins(const char* pluginpath, const char* which)
 {
-	setenv(PLUGINDIR_VAR, pluginpath, 1);
+	options_load(0, NULL, NULL, NULL);
+        options_set_opt("lircd:plugindir", pluginpath);
 	for_each_plugin(format_plugin, (void*)which);
 	qsort(lines, line_ix, sizeof(line_t*), line_cmp);
 	if (opt_summary) {
