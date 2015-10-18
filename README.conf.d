74ff2480c293be9fdb3efa0f1bbefe27b39fa4aa
Alec Leamas <leamas.alec@gmail.com>
2015-10-18 22:00:13 +0200

    Create a devinput-based default setup (#106).
    Using the devinput driver with the 'auto' device as default setup works
    if there is a single remote with rc-based driver. While certainly not
    covering all cases, it's a reasonable attempt - there is actually a
    chance this "works" out of the box for some users.

--- README.conf.d
+++ README.conf.d
@@ -1,31 +1,14 @@
 /etc/lirc/lircd.conf.d README
 =============================
 
-Files in this directory are automatically included, much the same way
-as if there was a "include" directive for it in lircd.conf. For this to
-work, the filename must match '*.conf'.
+Files in this directory are as distributed included by the main
+/etc/lirc/lircd.conf. Only files matching '*.conf' are included.
 
-As distributed, here is a single file devinput.lircd.dist. This should
-be used with the devinput driver; in this case copy it to
-devinput.lircd.conf.
+As distributed, here is a single file devinput.lircd.conf. This should
+be used with the devinput driver which is enabled by default. When using
+another driver, disable the devinput file e. g., by renaming it to
+devinput.lircd.dist.
 
 lircd normally (tries to) sort the remotes so the ones which decodes fastest
-are used first. If you want to sort your configs manually you can add a file
-like this to lircd.conf.d (the filename does not matter):
-
-       begin remote
-            name manual_sort
-            manual_sort 1
-            begin codes
-            end codes
-       end remote
-
-In manual sort mode the files here are used in the order they are listed by
-ls(1). In this situation it makes a lot of sense to name the files like:
-
-    00-manual-sort.conf
-    01-my-first-remote.conf
-    02-my-next-remote.conf
-    ...
-
-to define the sort order.
+are used first. If you want to sort your configs manually, see the
+Configuration Guide on using multiple remotes.
