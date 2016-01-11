53095bdb46e14273e3565ada728f730197975d10
Alec Leamas <leamas.alec@gmail.com>
2016-01-11 21:32:52 +0100

    Fix generation of plugin docs.

--- doc/plugindocs.mk
+++ doc/plugindocs.mk
@@ -5,28 +5,32 @@
 #
 
 HERE         = $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
-THERE        = $(abspath $(HERE)/../lirc.org/html)
-SRC          = $(filter-out $(HERE)/programs.html, $(wildcard $(HERE)/*.html))
+THERE        = $(HERE)/var
+INDEX        = $(THERE)/index.html
+SRC          = $(wildcard $(HERE)/*.html)
 DOCS         = $(subst $(HERE), $(THERE), $(SRC))
-INDEX        = $(THERE)/programs.html
-STYLESHEETS  = $(HERE)/plugpage.xsl \
-               $(HERE)/page.xsl \
-               $(HERE)/driver-toc.xsl \
-               $(HERE)/ext-driver-toc.xsl
+STYLESHEETS  = $(HERE)/plugpage.xsl $(HERE)/page.xsl \
 
+all: update
 
 update: $(DOCS) $(INDEX)
 
-
-clean-docs:
-	rm -f $(DOCS)
-
-$(HERE)/ext-driver-toc.xsl: $(HERE)/make-ext-driver-toc.sh $(SRC)
-	cd $(HERE); /bin/bash make-ext-driver-toc.sh $(SRC)> $@
-
 $(DOCS): $(SRC) $(STYLESHEETS)
-	cd $(HERE); \
-	    xsltproc --html plugpage.xsl $(subst $(THERE),$(HERE),$@) > $@
+	if test -n "$(SRC)"; then \
+	    sh $(HERE)/make-ext-driver-toc.sh $(SRC) \
+	        > $(THERE)/ext-driver-toc.xsl; \
+	    xsltproc --html $(HERE)/plugpage.xsl \
+	        $(subst $(THERE), $(HERE), $@) > $@; \
+	fi
 
-$(INDEX):$(HERE)/programs.html $(STYLESHEETS) $(SRC)
-	cd $(HERE); xsltproc --html plugpage.xsl programs.html > $@
+$(THERE)/ext-driver-toc.xsl: $(SRC)
+	sh $(HERE)/make-ext-driver-toc.sh $(SRC) > $@
+
+$(INDEX): $(HERE)/index.tmpl $(STYLESHEETS) $(SRC)
+	if [ -n "$(SRC)" ]; then \
+	    sh $(HERE)/make-ext-driver-toc.sh $(SRC) \
+	        > $(THERE)/ext-driver-toc.xsl; \
+	    xsltproc --html $(HERE)/plugpage.xsl $(HERE)/index.tmpl > $@; \
+	else \
+	    cp empty_index.tmpl $@; \
+	fi
