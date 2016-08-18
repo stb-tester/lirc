stb-tester/lirc

commit 82305c72c497c2884a7cdbfd1f9205049713c6eb

plugins, lircmd: Kill some compiler warnings.

plugins/audio_alsa.c
@@ -377,11 +377,11 @@ var_reset:			/* Reset variables */
 		for (i = 0; i < count; i++) {
 			/* cs == current sample */
 			unsigned char cs, as, sl, sz, xz;
-
+		        short stmp;
 			if (bytes_per_sample == 2) {
-				cs = ((*(short *)
-				       &buff[i * bytes_per_sample * alsa_hw.num_channels +
-					     bytes_per_sample * alsa_hw.channel]) >> 8);
+				stmp = buff[i * bytes_per_sample * alsa_hw.num_channels +
+					     bytes_per_sample * alsa_hw.channel];
+				cs = stmp >> 8;
 				cs ^= 0x80;
 			} else {
 				cs = buff[i];

daemons/lircmd.c
@@ -844,7 +844,7 @@ void loop()
 			}
 		}
 		buffer[len + end_len] = 0;
-		ret = sscanf(buffer, "%*llx %x %s %s\n", &rep, button, remote);
+		ret = sscanf(buffer, "%*x %x %s %s\n", &rep, button, remote);
 		end = strchr(buffer, '\n');
 		if (end == NULL) {
 			end_len = 0;

plugins/default.c
@@ -244,7 +244,7 @@ int default_init()
 		}
 
 	} else if (hw.rec_mode == LIRC_MODE_LIRCCODE) {
-		if (default_ioctl(LIRC_GET_LENGTH, &hw.code_length) == -1) {
+		if (default_ioctl(LIRC_GET_LENGTH, (void*) &hw.code_length) == -1) {
 			logprintf(LOG_ERR, "could not get code length");
 			logperror(LOG_ERR, "default_init()");
 			default_deinit();

plugins/ftdi.c
@@ -71,6 +71,7 @@ extern struct ir_remote *repeat_remote;
 static int pipe_main2tx[2] = { -1, -1 };
 static int pipe_tx2main[2] = { -1, -1 };
 
+#if 0
 static lirc_t time_left(struct timeval *current,struct timeval *last, lirc_t gap)
 {
 	__u32 secs,diff;
@@ -81,6 +82,7 @@ static lirc_t time_left(struct timeval *current,struct timeval *last, lirc_t gap
 
 	return((lirc_t) (diff<gap ? gap-diff:0));
 }
+#endif
 
 static void parsesamples(unsigned char *buf, int n, int pipe_rxir_w)
 {

plugins/hiddev.c
@@ -804,9 +804,8 @@ char *samsung_rec(struct ir_remote *remotes)
  */
 char *sonyir_rec(struct ir_remote *remotes)
 {
-	struct hiddev_event ev[4];
+	struct hiddev_event ev;
 	int rd;
-	int i;
 	unsigned char msg[16];
 
 	LOGPRINTF(1, "sonyir_rec");
@@ -830,11 +829,11 @@ char *sonyir_rec(struct ir_remote *remotes)
 	}
 
 	// Construct event
-	ev[i].value = (msg[3] << 16) | (msg[4] << 8) | ((msg[2]&0x7f) << 0);
+	ev.value = (msg[3] << 16) | (msg[4] << 8) | ((msg[2]&0x7f) << 0);
 
 	pre_code_length = 0;
 	pre_code = 0;
-	main_code = ev[0].value;
+	main_code = ev.value;
 	repeat_state = RPT_NO;
 
 	return decode_all(remotes);

plugins/irman_drv.c
@@ -28,7 +28,7 @@
 #include <sys/types.h>
 #include <irman.h>
 
-#include "lirc_driver.h"*
+#include "lirc_driver.h"
 #include "lirc/serial.h"
 
 extern struct ir_remote *repeat_remote, *last_remote;

commit 3dd939697e1fd226ac2fbfa0d8d9aaefd53e0042

plugins: audio_alsa: Fix byte truncating in 16-bit data (#218):
This fixes a bug introduced in 82305c72 which basically was
about muting a "dereferencing type-punned pointer will break
strict-aliasing rules" compiler warning.

plugins/audio_alsa.c
@@ -390,11 +390,12 @@ var_reset:                      /* Reset variables */
 		for (i = 0; i < count; i++) {
 			/* cs == current sample */
 			unsigned char cs, as, sl, sz, xz;
-			short stmp;
+			unsigned short stmp;
 
 			if (bytes_per_sample == 2) {
-				stmp = buff[i * bytes_per_sample * alsa_hw.num_channels +
-					    bytes_per_sample * alsa_hw.channel];
+				int ix = i * bytes_per_sample * alsa_hw.num_channels +
+					     bytes_per_sample * alsa_hw.channel;
+				memcpy(&stmp, &buff[ix], sizeof(stmp));
 				cs = stmp >> 8;
 				cs ^= 0x80;
 			} else {