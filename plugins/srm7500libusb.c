4a7359eb9884b4b75793d3ca4fc13f3a9472bfef
Henning Glawe <glaweh@debian.org>
2017-07-30 22:30:16 +0200

    plugins: srm7500: Add 500 us sleep (usb_control_msg fix).
    Add some 500 microsecond sleeps in the hope to resolve the
    usb_control_msg issue

--- plugins/srm7500libusb.c
+++ plugins/srm7500libusb.c
@@ -467,6 +467,7 @@ open_dev_sequence:
 	} else {
 		log_notice("usb dev_init_01 %p, %d", dev_handle, res);
 	}
+	SLEEP_NANO(500*1000); // wait for 500 microseconds
 
 	control_buffer[0] = 0xe4;
 	res = usb_control_msg(dev_handle, USB_TYPE_CLASS | USB_RECIP_INTERFACE | USB_ENDPOINT_OUT, USB_REQ_SET_CONFIGURATION,
@@ -476,6 +477,7 @@ open_dev_sequence:
 	} else {
 		log_notice("usb dev_init_02 %p, %d", dev_handle, res);
 	}
+	SLEEP_NANO(500*1000); // wait for 500 microseconds
 
 	control_buffer[0] = 0;
 	res = usb_control_msg(dev_handle, USB_TYPE_CLASS | USB_RECIP_INTERFACE | USB_ENDPOINT_IN, USB_REQ_CLEAR_FEATURE,
@@ -485,6 +487,7 @@ open_dev_sequence:
 	} else {
 		log_notice("usb dev_init_03 %p, %d", dev_handle, res);
 	}
+	SLEEP_NANO(500*1000); // wait for 500 microseconds
 
 	/*
 	 * HG: with this the red control light on usb receiver is
@@ -499,6 +502,7 @@ open_dev_sequence:
 	} else {
 		log_notice("usb dev_init_04 %p, %d", dev_handle, res);
 	}
+	SLEEP_NANO(500*1000); // wait for 500 microseconds
 
 	memset(control_buffer, 0, sizeof(control_buffer));
 	res = usb_control_msg(dev_handle, USB_TYPE_CLASS | USB_RECIP_INTERFACE | USB_ENDPOINT_IN, USB_REQ_CLEAR_FEATURE,
@@ -508,6 +512,7 @@ open_dev_sequence:
 	} else {
 		log_notice("usb dev_init_05 %p, %d", dev_handle, res);
 	}
+	SLEEP_NANO(500*1000); // wait for 500 microseconds
 
 	return 1;
 }
