04e4807f5046f17cdd22f5ab44a95e13663a5571
David Röthlisberger <david@stb-tester.com>
2019-10-28 14:45:57 +0000

    test: Add simple integration test for sending signals with lircd
    Run the tests with:
    
        pytest-3 -vv test/test_release_mask.py
    
    I was confused about the interactions between lircd.c and transmit.c --
    they both manipulate `remote->repeat_countdown` and there are loads of
    global variables involved (such as `repeat_remote`). This test proves
    that if you set a count (with `irsend --count=... send_once`) it doesn't
    override `min_repeat` if it's smaller than `min_repeat`, and vice versa.
    This wasn't obvious to me from reading the code so I wrote a test to
    satisfy myself. I will add further tests to this file in a future
    commit, when I'll be adding a feature to lirc.
    
    I've used python because it's much easier than bash. I've used [pytest],
    which is a widely used Python testing framework, because it makes it
    easy to test lots of combinations, like I've done here with
    `parametrize`. You can install pytest from PyPI (`pip3 install pytest`)
    or from your Linux distro (on Ubuntu: `sudo apt install python3-pytest`).
    
    As far as I can tell the existing tests in `test/tests` are for testing
    signal *decoding*; and this is the first integration test (that actually
    runs `lircd` rather than `irsimsend` or some such) for testing
    signal *sending*.
    
    [pytest]: https://pytest.org/

---
+++ test/test_release_mask.py
@@ -0,0 +1,124 @@
+# Run with: pytest-3 -vv test/test_release_mask.py
+
+import os
+import subprocess
+import time
+from textwrap import dedent
+
+import pytest
+
+
+class Lircd(object):
+    def __init__(self, socket, output):
+        self.socket = socket
+        self.output = output
+
+    def irsend(self, *args, count=None):
+        if count is not None:
+            count_args = ["--count=%s" % count]
+        else:
+            count_args = []
+        subprocess.check_call(
+            [_find_file("../tools/irsend"),
+             "--device", self.socket,
+            ] + count_args + list(args))
+
+
+@pytest.fixture(scope="function")
+def lircd(tmpdir):
+    socket = tmpdir.join("lircd.sock").strpath
+    output = tmpdir.join("driver.out").strpath
+
+    p = subprocess.Popen(
+        [_find_file("../daemons/lircd"),
+         "--nodaemon",
+         "--plugindir", _find_file("../plugins/.libs"),
+         "--pidfile", tmpdir.join("lircd.pid").strpath,
+         "--driver=file",
+         "--device", output,
+         "--output", socket,
+         _find_file("test_release_mask.lircd.conf")])
+    for _ in range(10):
+        if p.poll() is not None or os.path.exists(socket):
+            break
+        time.sleep(0.1)
+    else:
+        assert False, "lircd failed to start up within 1s"
+    assert p.returncode is None, "lircd exited at startup"
+    print("Found %s" % socket)
+    try:
+        yield Lircd(socket, output)
+    finally:
+        p.terminate()
+        p.wait()
+
+
+@pytest.mark.parametrize(
+    "min_repeats_in_config,irsend_count,expected_signals",
+    [(0, None, 1),
+     (0, 1, 1),
+     (0, 2, 3),
+     (0, 5, 6),
+     (1, None, 2),
+     (1, 1, 2),
+     (1, 2, 3),
+     (1, 5, 6),
+     (2, None, 3),
+     (2, 1, 3),
+     (2, 2, 3),
+     (2, 5, 6),
+    ])
+def test_release_mask(lircd, min_repeats_in_config, irsend_count,
+                      expected_signals):
+
+    remote = "no_release_%i_repeats" % min_repeats_in_config
+    lircd.irsend("SEND_ONCE", remote, "KEY_1", count=irsend_count)
+    expected = single_signal * expected_signals
+    actual = open(lircd.output).read()
+    assert expected_signals == sum(
+        1 for line in actual.split("\n") if line == "space 90000")
+    assert expected == actual
+
+
+def _find_file(f, root=os.path.dirname(__file__)):
+    return os.path.join(root, f)
+
+
+single_signal = dedent("""\
+    pulse 417
+    space 278
+    pulse 167
+    space 278
+    pulse 167
+    space 778
+    pulse 167
+    space 278
+    pulse 167
+    space 278
+    pulse 167
+    space 278
+    pulse 167
+    space 278
+    pulse 167
+    space 278
+    pulse 167
+    space 278
+    pulse 167
+    space 278
+    pulse 167
+    space 611
+    pulse 167
+    space 444
+    pulse 167
+    space 611
+    pulse 167
+    space 278
+    pulse 167
+    space 278
+    pulse 167
+    space 278
+    pulse 167
+    space 444
+    pulse 167
+    space 90000
+    """)
